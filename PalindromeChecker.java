b08d92b9f6ab51071eaba610cd0964fe66ea29f5
Mahesh-619Zeno <mahesh.ghadei@techolution.com>
2025-08-28 17:37:15 +0530

    Create PalindromeChecker.java

---
+++ PalindromeChecker.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class PalindromeChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a word: ");
+        String input = scanner.nextLine();
+
+        String reversed = "";
+
+        // Reverse the string
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed += input.charAt(i);
+        }
+
+        // Compare original and reversed
+        if (input.equalsIgnoreCase(reversed)) {
+            System.out.println(input + " is a palindrome.");
+        } else {
+            System.out.println(input + " is not a palindrome.");
+        }
+
+        scanner.close();
+    }
+}
