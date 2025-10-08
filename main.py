cf18094c12d344657705ca87955a22afa8c3af70
Mahesh-619Zeno <mahesh.ghadei@techolution.com>
2025-10-08 21:09:55 +0530

    added filter

--- main.py
+++ main.py
@@ -12,7 +12,14 @@ def fetch_data():
         response.raise_for_status()
 
         users = response.json()
+        print(f"Fetched {len(users)} users from GitHub.")        
+
+        users = response.json()
         df = pd.DataFrame(users)
+
+        filtered_df = df[df['id'] > 10000].copy()
+        filtered_df['fetched_at'] = datetime.datetime.now()
+        
         print("Fetched users (DataFrame):")
         print(df[['login', 'id']])
 
