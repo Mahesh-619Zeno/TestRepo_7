65d851fb772c78199cf125e63bc1f4cee4730693
Mahesh-619Zeno <mahesh.ghadei@techolution.com>
2025-10-11 00:53:06 +0530

    resolved all critical cs issues

--- calculator.py
+++ calculator.py
@@ -1,12 +1,12 @@
-def add(x, y): return x + y
+def add(first_number, second_number): return first_number + second_number
 
 print("Select operation: 1.Add 2.Subtract 3.Multiply 4.Divide")
 choice = input("Enter choice (1/2/3/4): ")
-x = float(input("Enter first number: "))
-y = float(input("Enter second number: "))
+first_number = float(input("Enter first number: "))
+second_number = float(input("Enter second number: "))
 
 if choice == '1':
-    print(f"Result: {add(x, y)}")
+    print(f"Result: {add(first_number, second_number)}")
 
 else:
     print("Invalid input")
