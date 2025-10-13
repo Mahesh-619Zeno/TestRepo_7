f2b81d48322f8deb0919e22a1b530a7aec635b6a
Mahesh-619Zeno <mahesh.ghadei@techolution.com>
2025-10-13 17:22:03 +0530

    Resolved all critical issues

--- guessTheNum.py
+++ guessTheNum.py
@@ -1,12 +1,12 @@
 import random
 
-number = random.randint(1, 10)
+secret_number = random.randint(1, 10)
 guess = 0
-while guess != number:
+while guess != secret_number:
     guess = int(input("Guess a number between 1 and 10: "))
-    if guess < number:
+    if guess < secret_number:
         print("Too low!")
-    elif guess > number:
+    elif guess > secret_number:
         print("Too high!")
     else:
         print("Correct!")
